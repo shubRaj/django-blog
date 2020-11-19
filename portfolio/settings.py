81c9ce9e40d76a4f067c4c495a09f4c468c19431
Shub Raj Lama <shuvraj1234@gmail.com>
2020-11-19 15:01:59 +0545

    requirement updated

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,20 +11,19 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v09c+a0$-ph2zogboi1vt@ekpc=8w(z$jsi#j6z#pc!@djrpe8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["shubraj.herokuapp.com",]
 
 
 # Application definition
@@ -82,8 +81,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'PORT':os.environ.get("DB_PORT"),
+        'HOST':os.environ.get("DB_HOST"),
+        "USER":os.environ.get("DB_NAME"),
+        "PASSWORD":os.environ.get("DB_PASSWORD")
     }
 }
 
