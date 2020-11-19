a225ee75c2cf0a0caf7fd59d9fc61c0bc1cc0b7d
Shub Raj Lama <shuvraj1234@gmail.com>
2020-11-19 16:05:31 +0545

    requirement updated

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path("",include('resume.urls',namespace="resume")),
 ]
 if settings.DEBUG:
-    urlpatterns+=path("__debug__",include(debug_toolbar.urls))+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns+=path("__debug__",include(debug_toolbar.urls))
 else:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     handler500 = 'portfolio.handlers.server_error'
     handler404 = 'portfolio.handlers.page_not_found'
